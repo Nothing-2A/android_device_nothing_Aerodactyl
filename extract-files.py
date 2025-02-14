764d413119c41db036b9b4a4555887d7da0b41f5
Giovanni Ricca <giovanniricca@proton.me>
2025-02-14 15:52:56 +0100

    Pacman: Enable ELF checks for `libneuralnetworks_sl_driver_mtk_prebuilt`
    Change-Id: Ia33879798e16e8724a93e6c4f6a0cac874858785

--- extract-files.py
+++ extract-files.py
@@ -60,6 +60,14 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/mt6886/libmorpho_video_stabilizer.so': blob_fixup()
         .add_needed('libutils.so'),
+    'vendor/lib64/mt6886/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
