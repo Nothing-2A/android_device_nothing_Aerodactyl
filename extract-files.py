c78a7cba6f34a092c80982542bb9436542ad949f
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 14:01:57 +0200

    Pacman: Shim Nt charge service with `libbase_shim.so`

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.power-V3-ndk.so', 'android.hardware.power-V2-ndk.so'),
     'vendor/bin/hw/vendor.noth.hardware.camera-service': blob_fixup()
         .add_needed('libui_shim.so'),
+    'vendor/bin/hw/vendor.noth.hardware.charge-service': blob_fixup()
+        .add_needed('libbase_shim.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v33.so'),
 }  # fmt: skip
