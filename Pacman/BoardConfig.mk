ad8f0053ee91147b6ceda78b3a7690e657138d7f
Mashopy <eliasgheeraert@gmail.com>
2025-07-03 18:13:30 +0200

    Aerodactyl: Pacman: Initial BoardConfig

---
+++ Pacman/BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include the common BoardConfig.
+include device/nothing/Aerodactyl/BoardConfig-common.mk
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := Pacman
