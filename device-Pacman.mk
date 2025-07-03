a8f38372b44dc68052542b09c73e36566f2f215e
Mashopy <eliasgheeraert@gmail.com>
2025-07-03 18:19:37 +0200

    Aerodactyl: Move device-common.mk inclusion into device-$(TARGET).mk

---
+++ device-Pacman.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the custom common configuration.
+$(call inherit-product, device/nothing/Aerodactyl/device-common.mk)
