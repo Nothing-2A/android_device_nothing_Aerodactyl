d1a453d4aac14367eba40eb8402d8ece737239f1
Mashopy <eliasgheeraert@gmail.com>
2025-08-18 21:22:04 +0200

    Aerodactyl: init: Checkout init.insmod.sh from MediaTek k61 mgvi
    k61 targets natively supports system_dlkm modprobe

--- init/init.insmod.sh
+++ init/init.insmod.sh
@@ -18,16 +18,32 @@ if [ -f $cfg_file ]; then
   do
     case $action in
       "insmod") insmod $arg ;;
-      "setprop") setprop $arg 1 ;;
+      "setprop")
+        times=1
+        setprop $arg 1
+        while [ "$?" -ne 0 ]
+        do
+          if [ $times -gt 128 ]; then
+            break
+          fi
+          let times++
+          setprop $arg 1
+        done ;;
       "enable") echo 1 > $arg ;;
       "modprobe")
-        case ${arg} in
-          "-b *" | "-b")
-            arg="-b $(cat /vendor_dlkm/lib/modules/modules.load)" ;;
-          "*" | "")
-            arg="$(cat /vendor_dlkm/lib/modules/modules.load)" ;;
-        esac
-        modprobe -a -d /vendor_dlkm/lib/modules $arg ;;
+        insmod_arg=${arg}
+        for partition in system_dlkm vendor_dlkm
+        do
+          case ${insmod_arg} in
+            "-b *" | "-b")
+              arg="-b $(cat /${partition}/lib/modules/modules.load)" ;;
+            "*" | "")
+              arg="$(cat /${partition}/lib/modules/modules.load)" ;;
+          esac
+          modprobe -a -d /${partition}/lib/modules $arg
+        done
     esac
   done < $cfg_file
+else
+  exit 2
 fi
