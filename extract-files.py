c085efd0e82fd3ab11618abd3a4b8b2e8a1bcb82
Mashopy <eliasgheeraert@gmail.com>
2025-08-21 09:34:13 +0200

    Aerodactyl: Use the correct RKP interface for keymint service
    RKP V2 got introduced along with Keymint V2 on Android 13

--- extract-files.py
+++ extract-files.py
@@ -54,7 +54,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.trustonic': blob_fixup()
-        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+        .add_needed('android.hardware.security.rkp-V2-ndk.so'),
     'vendor/bin/hw/mt6886/camerahalserver': blob_fixup()
         .add_needed('libcamera_metadata_shim.so'),
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
