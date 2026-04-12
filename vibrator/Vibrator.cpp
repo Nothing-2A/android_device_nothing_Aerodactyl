a6f24b5184be10232d31ed55f9d711c6cffe9473
darkDolphin1 <shravanrajapure1@gmail.com>
2026-04-12 07:22:43 +0000

    Aerodactyl: vibrator: tune down haptics strength

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -13,9 +13,9 @@
 
 #include "aac_vibra_function.h"
 
-#define RICHTAP_LIGHT_STRENGTH 77
-#define RICHTAP_MEDIUM_STRENGTH 88
-#define RICHTAP_STRONG_STRENGTH 100
+#define RICHTAP_LIGHT_STRENGTH 25
+#define RICHTAP_MEDIUM_STRENGTH 45
+#define RICHTAP_STRONG_STRENGTH 77
 
 enum vibrationMode {
     MODE_NONE,
